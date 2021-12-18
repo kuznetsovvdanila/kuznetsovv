5f934136087bd0742d3f3fbfc13826a9cdb22f2e
kuznetsovvdanila <kuznetsovvdanila@gmail.com>
2021-12-18 23:44:25 +0300

    deploy: static dir changes

--- kuznetsovv/kuznetsovv/settings.py
+++ kuznetsovv/kuznetsovv/settings.py
@@ -138,8 +138,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
 BASE_DIR = Path(__file__).resolve().parent.parent
+print(BASE_DIR)
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
